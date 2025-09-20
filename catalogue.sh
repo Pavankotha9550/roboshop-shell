d2939aa9138f76130a5a3d62c550720c476be560
pavankotha <pavankotha9550@outlook.com>
2025-09-20 12:21:02 +0530

    aws cli instances creation file updated created testing now

---
+++ catalogue.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+W="\e[0m"
+
+echo -e "script started executed at $y$(date)$W"
+
+log_folder="var/log/shell-scripting-logs"
+script_name=$(echo $0 | cut -d "." -f1)
+log_file=$log_folder/$script_name.log
+mkdir -p $log_folder
+
+echo "userid:$userid"
+if [ $userid -ne 0 ] 
+then
+    echo -e "$R error: not sudo user $W" | tee -a $log_file
+    exit 1
+else
+    echo -e "$G running with sudo user $W" | tee -a $log_file
+fi
+
+
+VALIDATE()
+{
+     if [ $1 -eq 0 ]
+    then
+        echo -e "$G installation $2 is success $W" | tee -a $log_file
+    else
+        echo -e "$R error: installation $2 failed $W" | tee -a $log_file
+        exit 1
+    fi
+}
+
+dnf module disable nodejs -y &>>$log_file
+VALIDATE $? "disabling nodejs"
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "enabling nodejs 20"
+
+dnf install nodejs -y
+VALIDATE $? "installing nodejs"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+VALIDATE $? "adding roboshop user"
+
+mkdir /app 
+VALIDATE $? "making app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+VALIDATE $? "downlading zip is success"
+
+cd /app 
+VALIDATE $? "moving to app directory"
+
+unzip /tmp/catalogue.zip
+VALIDATE $? "unziping catalogue zip in app"
+
+npm install 
+VALIDATE $? "installing npm dependendies"
+
+sed -i 's/<MONGODB-SERVER-IPADDRESS>/mongodb.daws84.cyou/' catlog.services
+cp catlog.services /etc/systemd/system/catalogue.service
+VALIDATE $? "copying catalogue. services is done"
+
+systemctl daemon-reload
+VALIDATE $? "reloading of services"
+
+systemctl enable catalogue 
+VALIDATE $? "enabiling catalogue"
+
+systemctl start catalogue
+VALIDATE $? "start catalogue"
+
+dnf install mongodb-mongosh -y
+VALIDATE $? "installing mongodb client"
+
+mongosh --host MONGODB-SERVER-IPADDRESS </app/db/master-data.js
+VALIDATE $? "loading masterdata to db"
+
