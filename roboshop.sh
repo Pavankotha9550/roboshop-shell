9dc5325a193f38ffaa604d6301624831a8547d09
pavankotha <pavankotha9550@outlook.com>
2025-09-20 10:58:33 +0530

    aws cli instances creation file updated created testing now

--- roboshop.sh
+++ roboshop.sh
@@ -11,10 +11,33 @@ do
     instance_id=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0ccfe8392d60403ed --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instances}]" --query "Instances[0].InstanceId" --output text)
     
     if [ $instances != "frontend" ]
-    then
-       ip=$(aws ec2 describe-instances --instance-ids $instance_id --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-    else
-       ip=$(aws ec2 describe-instances --instance-ids $instance_id --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        then
+        ip=$(aws ec2 describe-instances --instance-ids $instance_id --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        else
+        ip=$(aws ec2 describe-instances --instance-ids $instance_id --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+            aws route53 change-resource-record-sets \
+        --hosted-zone-id $zone_id \
+        --change-batch '
+        {
+        "Comment": "Update A record for iinstances created",
+        "Changes": [
+            {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'$domain_name'",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                {
+                    "Value": "'$ip'"
+                }
+                ]
+            }
+            }
+        ]
+        }
+        '
+        
     fi
     echo "instance name : $instances and ip was: $ip"
 
