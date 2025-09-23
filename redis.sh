6c9461fa7a4b2ad0a53bd09710b9d58733f8210d
pavankotha <pavankotha9550@outlook.com>
2025-09-23 20:50:53 +0530

    modify redis file updated created testing now

--- redis.sh
+++ redis.sh
@@ -45,9 +45,9 @@ dnf install redis -y  &>>$log_file
 VALIDATE $? "insatlling redis"
 
 cd /etc/redis/
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
-VALIDATE $? "changing ip"
-sed -i 's/protected-mode yes/protected-mode no' /etc/redis/redis.conf
+#sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
+#VALIDATE $? "changing ip"
+sed -i 's/protected-mode yes/protected-mode no/' /etc/redis/redis.conf
 VALIDATE $? "changing protection mode"
 
 systemctl enable redis 
