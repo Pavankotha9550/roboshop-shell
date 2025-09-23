c0dc09e5f631123e56e95ec970236ce3f5fe1960
pavankotha <pavankotha9550@outlook.com>
2025-09-23 21:31:28 +0530

    user file updated created testing now

---
+++ user.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+W="\e[0m"
+
+echo -e "script started executed at $y$(date)$W"
+
+log_folder="var/log/shell-scripting-logs"
+script_name=$(echo $0 | cut -d "." -f1)
+log_file=$log_folder/$script_name.log
+mkdir -p $log_folder
+
+echo "userid:$userid"
+if [ $userid -ne 0 ] 
+then
+    echo -e "$R error: not sudo user $W" | tee -a $log_file
+    exit 1
+else
+    echo -e "$G running with sudo user $W" | tee -a $log_file
+fi
+
+
+VALIDATE()
+{
+     if [ $1 -eq 0 ]
+    then
+        echo -e "$G installation $2 is success $W" | tee -a $log_file
+    else
+        echo -e "$R error: installation $2 failed $W" | tee -a $log_file
+        exit 1
+    fi
+}
+
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+VALIDATE $? "disabiling and enabiling nodejs"
+
+dnf install nodejs -y
+VALIDATE $? "installing nodejs"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+VALIDATE $? "adding roboshop user"
+
+mkdir /app 
+VALIDATE $? "creating app directory"
+
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
+VALIDATE $? "downloading user files"
+
+rm -rf /app/*
+cd /app 
+VALIDATE $? "moving to app directory"
+
+unzip /tmp/user.zip
+VALIDATE $? "unzipping user files"
+
+npm install 
+VALIDATE $? "installing npm dependendies"
+
+cp $PWD/user.service /etc/systemd/system/user.service
+sed -i 's/<REDIS-IP-ADDRESS>/redis.daws84.cyou/'  /etc/systemd/system/user.service
+VALIDATE $? "changing ip of redis"
+sed -i 's/<MONGODB-SERVER-IP-ADDRESS>/mongodb.daws84.cyou/' /etc/systemd/system/user.service
+VALIDATE $? "changing ip of mongo"
+
+systemctl daemon-reload
+VALIDATE $? "reloading user.service"
+
+systemctl enable user 
+VALIDATE $? "enabiling  user"
+systemctl start user
+VALIDATE $? "starting user"
