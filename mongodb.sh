7eb8a25fb374b069ca5a64e1bd74ce17748afa5f
pavankotha <pavankotha9550@outlook.com>
2025-09-24 22:24:13 +0530

    catalogue file updated created testing now

--- mongodb.sh
+++ mongodb.sh
@@ -1,39 +1,4 @@
-#!/bin/bash
-
-userid=$(id -u)
-
-R="\e[31m"
-G="\e[32m"
-y="\e[33m"
-W="\e[0m"
-
-echo -e "script started executed at $y$(date)$W"
-
-log_folder="var/log/shell-scripting-logs"
-script_name=$(echo $0 | cut -d "." -f1)
-log_file=$log_folder/$script_name.log
-mkdir -p $log_folder
-
-echo "userid:$userid"
-if [ $userid -ne 0 ] 
-then
-    echo -e "$R error: not sudo user $W" | tee -a $log_file
-    exit 1
-else
-    echo -e "$G running with sudo user $W" | tee -a $log_file
-fi
-
-
-VALIDATE()
-{
-     if [ $1 -eq 0 ]
-    then
-        echo -e "$G installation $2 is success $W" | tee -a $log_file
-    else
-        echo -e "$R error: installation $2 failed $W" | tee -a $log_file
-        exit 1
-    fi
-}
+source ./common.sh
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "copying mongo db repo"
