1ebe245d1f56a827b7afc56386c936a006d3c314
pavankotha <pavankotha9550@outlook.com>
2025-09-24 22:42:04 +0530

    catalogue file updated created testing now

---
+++ common.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+W="\e[0m"
+
+start-time=$(date)
+echo -e "script started executed at $y$(date)$W"
+
+log_folder="/var/log/roboshop-shell"
+mkdir -p $log_folder
+script_name=$(echo $0 | cut -d "." -f1)
+log_file=$log_folder/$script_name.log
+touch $log_file
+script_dir=$PWD
+
+echo "userid:$userid"
+if [ $userid -ne 0 ] 
+then
+    echo -e "$R error: not sudo user $W" | tee -a $log_file
+    exit 1
+else
+    echo -e "$G running with sudo user $W" | tee -a $log_file
+fi
+
+
+VALIDATE()
+{
+     if [ $1 -eq 0 ]
+    then
+        echo -e "$G installation $2 is success $W" | tee -a $log_file
+    else
+        echo -e "$R error: installation $2 failed $W" | tee -a $log_file
+        exit 1
+    fi
+}
